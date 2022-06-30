410213ffea693d1e2393943872e893c8ea3fe0ef
Денис Середин <sibwinter42@gmail.com>
2022-07-01 00:25:57 +0300

    все тесты пройдены, бот абы как работает, отправляю

--- homework.py
+++ homework.py
@@ -1,4 +1,3 @@
-import json
 from logging.handlers import RotatingFileHandler
 import time
 import logging
@@ -18,7 +17,9 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = RotatingFileHandler('bot.log', maxBytes=50000000, backupCount=5)
 logger.addHandler(handler)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
 handler.setFormatter(formatter)
 
 PRACTICUM_TOKEN = 'AQAAAAAPbkzJAAYckR9Mt0AI-EfAks6ieORiRQQ'
@@ -53,56 +54,47 @@ def get_api_answer(current_timestamp):
         if response.status_code != 200:
             raise requests.exceptions.RequestException 
         return response.json()
-    except requests.exceptions.RequestException as e:  # This is the correct syntax
+    except requests.exceptions.RequestException as e:
         raise SystemExit(e)
-
-
 
 
 def check_response(response):
     if response == {}:
         raise TypeError('Пустой словарь в ответе')
-
-
     if (isinstance(response, dict) and 
-        isinstance(response['homeworks'], list)):
+       isinstance(response['homeworks'], list)):
         return response['homeworks']
     else:
         raise TypeError('Unable to parse response, invalid JSON.')
 
 
-
-
 def parse_status(homework):
     homework_name = homework['homework_name']
     homework_status = homework['status']
-
-
     verdict = HOMEWORK_STATUSES[homework_status]
-
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    if (PRACTICUM_TOKEN == None or 
-        TELEGRAM_TOKEN == None or 
-        TELEGRAM_CHAT_ID == None or 
+    if (
+        PRACTICUM_TOKEN is None or 
+        TELEGRAM_TOKEN is None or 
+        TELEGRAM_CHAT_ID is None or 
         PRACTICUM_TOKEN == '' or 
         TELEGRAM_TOKEN == '' or 
-        TELEGRAM_CHAT_ID == ''):
-        logger.critical('отсутствуют обязательные переменные окружения во время запуска бота')
+        TELEGRAM_CHAT_ID == ''
+    ):
+        logger.critical('отсутствуют обязательные переменные окружения')
         return False
     else:
         return True
 
 
-
 def main():
     """Основная логика работы бота."""
 
-
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time()- 60*60*24*30)
+    current_timestamp = int(time.time() - 60 * 60 * 24 * 30)
     previouse_status = ''
 
     while True:
@@ -115,7 +107,7 @@ def main():
                 send_message(bot, current_status)
 
             time.sleep(RETRY_TIME)
-            current_timestamp = int(time.time()- 60*60*24*30)         
+            current_timestamp = int(time.time() - 60 * 60 * 24 * 30)         
             previouse_status = current_status
 
         except Exception as error:
